1e4fb0a3a1f4f9de03ac62a8a3386dad775dd0bc
Matt Gill <mtgill90@outlook.com>
2019-05-28 18:19:44 -0500

    auth setup complete

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,17 @@
+import firebase from 'firebase/app';
 import 'bootstrap';
 import '../styles/main.scss';
+import auth from './components/auth/auth';
+import MyNavBar from './components/MyNavbar/MyNavbar';
+import authData from './helpers/data/authData';
 
-console.error('main.js');
+import apiKeys from './helpers/apiKeys.json';
+
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  MyNavBar.navbarEvents();
+  authData.checkLoginStatus();
+  auth.authStringBuilder();
+};
+
+init();
