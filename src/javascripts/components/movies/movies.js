52c9a0e1c2d6fe2ce6ca38d646163f0b6af1c07a
Matt Gill <mtgill90@outlook.com>
2019-05-30 18:49:19 -0500

    movie cards and buttons displaying to dom

--- src/javascripts/components/movies/movies.js
+++ src/javascripts/components/movies/movies.js
@@ -7,10 +7,12 @@ const movieCardBuilder = () => {
       let domString = '';
       movies.forEach((movie) => {
         domString += '<div class="col-sm-12 col-md-6 col-lg-4 card-group">';
-        domString += '<div class="card">';
+        domString += '<div class="card movie-card">';
         domString += `<h3 class="card-title">${movie.title}</h3>`;
         domString += `<img src="${movie.imageUrl}" class="img-fluid movie-image" alt="movie photo" />`;
-        domString += `<h6 class="card-info">MPAA Rating: ${movie.movieRating}</h6>`;
+        domString += '<button class="btn btn-info">Add To Watchlist</button>';
+        domString += '<button class="btn btn-warning">Rate This Movie</button>';
+        domString += `<h4 class="card-info">MPAA Rating: ${movie.movieRating}</h4>`;
         domString += '</div>';
         domString += '</div>';
       });
